7658db7907bf6cca58e625a6c04a045696f99283
Mathilda <mathilda.magnusson@gmail.com>
2023-10-06 13:27:00 +0200

    modify works

--- portfolio/app.js
+++ portfolio/app.js
@@ -41,15 +41,6 @@ app.get("/", function (request, response) {
   response.render("about.handlebars", model);
 });
 
-/* app.get("/projects", function (request, response) {
-  const model = {
-    isLoggedIn: request.session.isLoggedIn,
-    name: request.session.name,
-    isAdmin: request.session.isAdmin,
-  };
-  response.render("projects.handlebars", model);
-}); */
-
 app.get("/projects", (request, response) => {
   db.all("SELECT * FROM projects", function (error, theProjects) {
     if (error) {
@@ -129,14 +120,106 @@ app.post(`/login`, (request, response) => {
   }
 });
 
+//ADD THE TABLES
+db.run(
+  "CREATE TABLE projects (pid INTEGER PRIMARY KEY, pname TEXT NOT NULL, pyear INTEGER NOT NULL, pdesc TEXT NOT NULL, ptype TEXT NOT NULL, pimgURL TEXT NOT NULL)",
+  (error) => {
+    if (error) {
+      // tests error: display error
+      console.log("ERROR: ", error);
+    } else {
+      // tests error: no error, the table has been created
+      console.log("---> Table projects created!");
+
+      const projects = [
+        {
+          id: "1",
+          name: "Pippi Långstrum Illustration",
+          year: 2022,
+          desc: "This is an illustration of pippi",
+          type: "illustrator",
+          url: "/img/Me.jpg",
+        },
+        {
+          id: "2",
+          name: "djur",
+          year: 2022,
+          desc: "This is an illustration of an animal",
+          type: "illustrator",
+          url: "/img/Me.jpg",
+        },
+        {
+          id: "3",
+          name: "Catch the students JavaScript game",
+          year: 2023,
+          desc: "This is a game made with javascript",
+          type: "Programming",
+          url: "/img/Me.jpg",
+        },
+      ];
+      // inserts projects
+      projects.forEach((oneProject) => {
+        db.run(
+          "INSERT INTO projects (pid, pname, pyear, pdesc, ptype, pimgURL) VALUES (?, ?, ?, ?, ?, ?)",
+          [
+            oneProject.id,
+            oneProject.name,
+            oneProject.year,
+            oneProject.desc,
+            oneProject.type,
+            oneProject.url,
+          ],
+          (error) => {
+            if (error) {
+              console.log("ERROR: ", error);
+            } else {
+              console.log("Line added into the projects table!");
+            }
+          }
+        );
+      });
+    }
+  }
+);
+
 //new project
 app.get("/projects/new", function (request, response) {
-  const model = {
-    isLoggedIn: request.session.isLoggedIn,
-    name: request.session.name,
-    isAdmin: request.session.isAdmin,
-  };
-  response.render("newProject.handlebars", model);
+  if (request.session.isLoggedIn == true && request.session.isAdmin == true) {
+    const model = {
+      isLoggedIn: request.session.isLoggedIn,
+      name: request.session.name,
+      isAdmin: request.session.isAdmin,
+    };
+    response.render("newProject.handlebars", model);
+  } else {
+    response.redirect("/login");
+  }
+});
+
+app.post("/projects/new", (request, response) => {
+  const newp = [
+    request.body.projname,
+    request.body.projyear,
+    request.body.projdesc,
+    request.body.projtype,
+    request.body.projimg,
+  ];
+  if (request.session.isLoggedIn == true && request.session.isAdmin == true) {
+    db.run(
+      "INSERT INTO projects (pname, pyear, pdesc, ptype, pimgURL) VALUES (?,?,?,?,?)",
+      newp,
+      (error) => {
+        if (error) {
+          console.log("ERROR:", error);
+        } else {
+          console.log("line added to projects table");
+        }
+        response.redirect("/projects");
+      }
+    );
+  } else {
+    response.redirect("/login");
+  }
 });
 
 //delete a project
@@ -173,217 +256,114 @@ app.get(`/projects/delete/:id`, (request, response) => {
   }
 });
 
-db.run(
-  "CREATE TABLE projects (pid INTEGER PRIMARY KEY, pname TEXT NOT NULL, pyear INTEGER NOT NULL, pdesc TEXT NOT NULL, ptype TEXT NOT NULL, pimgURL TEXT NOT NULL)",
-  (error) => {
-    if (error) {
-      // tests error: display error
-      console.log("ERROR: ", error);
-    } else {
-      // tests error: no error, the table has been created
-      console.log("---> Table projects created!");
+app.post("/projects/update/:id", (request, response) => {
+  const id = request.params.id;
+  const newp = [
+    request.body.projectName,
+    request.body.projectImg,
+    request.body.projectDesc,
+    request.body.projectDate,
+    request.body.userFK,
+    id,
+  ];
+  if (request.session.isLoggedIn == true && request.session.isAdmin == true) {
+    db.run(
+      "UPDATE projects SET projectName=?, projectImg=?, projectDesc=?, projectDate=?, userFK=?",
+      newp,
+      (error) => {
+        if (error) {
+          console.log("ERROR: ", error);
+        } else {
+          console.log("Project updated!");
+        }
+        response.redirect("/projects");
+      }
+    );
+  } else {
+    response.redirect("/login");
+  }
+});
 
-      const projects = [
-        {
-          id: "1",
-          name: "Counting people with a camera",
-          type: "research",
-          desc: "The purpose of this project is to count people passing through a corridor and to know how many are in the room at a certain time.",
-          year: 2022,
-          dev: "Python and OpenCV (Computer vision) library",
-          url: "/img/counting.png",
-        },
-        {
-          id: "2",
-          name: "Visualisation of 3D medical images",
-          type: "research",
-          desc: "The project makes a 3D model of the analysis of the body of a person and displays the detected health problems. It is useful for doctors to view in 3D their patients and the evolution of a disease.",
-          year: 2012,
-          url: "/img/medical.png",
-        },
-        {
-          id: "3",
-          name: "Multiple questions system",
-          type: "teaching",
-          desc: "During the lockdowns in France, this project was useful to test the students online with a Quizz system.",
-          year: 2021,
-          url: "/img/qcm07.png",
-        },
-        {
-          id: "4",
-          name: "Image comparison with the Local Dissmilarity Map",
-          desc: "The project is about finding and quantifying the differences between two images of the same size. The applications were numerous: satallite imaging, medical imaging,...",
-          year: 2020,
-          type: "research",
-          url: "/img/diaw02.png",
-        },
-        {
-          id: "5",
-          name: "Management system for students' internships",
-          desc: "This project was about the creation of a database to manage the students' internships.",
-          year: 2012,
-          type: "teaching",
-          url: "public/img/Me.jpg",
-        },
-      ];
-      // inserts projects
-      projects.forEach((oneProject) => {
-        db.run(
-          "INSERT INTO projects (pid, pname, pyear, pdesc, ptype, pimgURL) VALUES (?, ?, ?, ?, ?, ?)",
-          [
-            oneProject.id,
-            oneProject.name,
-            oneProject.year,
-            oneProject.desc,
-            oneProject.type,
-            oneProject.url,
-          ],
-          (error) => {
-            if (error) {
-              console.log("ERROR: ", error);
-            } else {
-              console.log("Line added into the projects table!");
-            }
-          }
-        );
-      });
+app.get("/projects/modify/:id", (request, response) => {
+  const id = request.params.id;
+  db.get(
+    "SELECT * FROM projects WHERE pid=?",
+    [id],
+    function (error, theProjects) {
+      if (error) {
+        console.log("ERROR:", error);
+        const model = {
+          dbError: true,
+          theError: error,
+          project: {},
+          isLoggedIn: request.session.isLoggedIn,
+          name: request.session.name,
+          isAdmin: request.session.isAdmin,
+        };
+        response.render("modifyProject.handlebars", model);
+      } else {
+        const model = {
+          dbError: false,
+          theError: "",
+          project: theProjects,
+          isLoggedIn: request.session.isLoggedIn,
+          name: request.session.name,
+          isAdmin: request.session.isAdmin,
+          helpers: {
+            theTypeI(value) {
+              return value == "Illustrator";
+            },
+            theTypePh(value) {
+              return value == "Photoshop";
+            },
+            theTypePr(value) {
+              return value == "Programming";
+            },
+            theTypeIP(value) {
+              return value == "Illustrator and Programming";
+            },
+            theTypeInD(value) {
+              return value == "InDesign";
+            },
+            theTypeInDI(value) {
+              return value == "InDesign and Illustrator";
+            },
+          },
+        };
+        response.render("modifyProject.handlebars", model);
+      }
     }
-  }
-);
-
-/* db.run(
-  "CREATE TABLE projects (pid INTEGER PRIMARY KEY, pname TEXT NOT NULL, pyear INTEGER NOT NULL, pdesc TEXT NOT NULL, ptype TEXT NOT NULL, pimgurl TEXT NOT NULL)",
-  (error) => {
-    if (error) {
-      console.log("error:", error);
-    } else {
-      console.log("---> table projects created");
-      const projects = [
-        {
-          id: "1",
-          pname: "Pippi Långstrum Illustration",
-          pyear: 2022,
-          pdesc: "This is an illustration of pippi",
-          ptype: "illustrator",
-          purl: "public/img/Me.jpg",
-        },
-        {
-          pid: "2",
-          pname: "djur",
-          pyear: 2022,
-          pdesc: "This is an illustration of an animal",
-          ptype: "illustrator",
-          purl: "public/img/Me.jpg",
-        },
-        {
-          pid: "3",
-          pname: "Catch the students JavaScript game",
-          pyear: 2023,
-          pdesc: "This is a game made with javascript",
-          ptype: "Code",
-          purl: "public/img/Me.jpg",
-        },
-      ];
-      projects.forEach((oneProject) => {
-        db.run(
-          "INSERT INTO projects (pid, pname, pyear, pdesc, ptype, pimgURL) VALUES (?, ?, ?, ?, ?, ?)",
-          [
-            oneProject.pid,
-            oneProject.pname,
-            oneProject.pyear,
-            oneProject.pdesc,
-            oneProject.ptype,
-            oneProject.pimgurl,
-          ],
-          (error) => {
-            if (error) {
-              console.log("error:", error);
-            } else {
-              console.log("line added to the projects table");
-            }
-          }
-        );
-      });
-    }
-  }
-); */
-
-//
-/* // MODEL (DATA)
-const humans = [
-  { id: 0, name: "Jerome" },
-  { id: 1, name: "Mira" },
-  { id: 2, name: "Linus" },
-  { id: 3, name: "Susanne" },
-  { id: 4, name: "Jasmin" },
-]; */
-/* 
-
-//lecture login
-passport.use(new localStorage(
-  function(username, password, done) {
-    //verify username and password
-  }
-));
-
-app.get("/", (request,response) => {
-  if (request.isAuthenticated() {
-
-  })
+  );
 });
 
-const bcrypt = require("bcrypt");
-const hash = bcrypt.hashSync(password,10);
-
-app.get("/" (request,response) => {
-  if (request.session.user) {
-    response.render("index", (user: {request.session.user}))
+app.post("/projects/modify/:id", (request, response) => {
+  const id = request.params.id;
+  const newp = [
+    request.body.projname,
+    request.body.projyear,
+    request.body.projdesc,
+    request.body.projtype,
+    request.body.projimg,
+    id,
+  ];
+  if (request.session.isLoggedIn == true && request.session.isAdmin == true) {
+    db.run(
+      "UPDATE projects SET pname=?, pyear=?, pdesc=?, ptype=?, pimgURL=? WHERE pid=?",
+      newp,
+      (error) => {
+        if (error) {
+          console.log("ERROR: ", error);
+        } else {
+          console.log("Project updated!");
+        }
+        response.redirect("/projects");
+      }
+    );
+  } else {
+    response.redirect("/login");
   }
 });
 
-
-
-
-//sessions with handlebars
-app.get("/" (request,response) => {
-
-    response.render("index",{user: request.session.user})
-  
-});
-//cookies res.cookie(name, value, options)
-res.cookie("theme", "dark",{ maxAge: 9000, httpOnlt: true});
-
-const cookieParser = require("cookie-parser");
-
-//cookie w handlebars get the cookie and send to handlebars
-app.get("/" (request,response) => {
-  response.render("index", {theme: request.cookies.theme})
-});
-
-const isAuthenticated = (req, res, next) => {
-if (req.isAuthenticated()) {
-next();
-} else {
-res.redirect('/login');
-}
-};
-
-app.get('/secret', isAuthenticated, (req, res) => {
-res.render('secret');
-});
-
-//in handlebars
-{{#if user}}
-h1 hallo {{user.username}}
-{{else}}
-h1 hello guest
-{{/if}}
-
-
-
-
-
-*/
 app.use(function (req, res) {
   res.status(404).render("404.handlebars");
 });
